506fbc8db6f7dc2d29878e2992f7c8cb5ea6b4d2
sylvainf112 <67450033+sylvainf112@users.noreply.github.com>
2020-06-26 12:39:56 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
+cd /
+cd opt
 sudo su
-sudo apt-get update && apt-get -y upgrade
+sudo apt-get update && apt-get upgrade -y
+sudo apt autoremove -y
 CUDA_REPO_PKG=cuda-repo-ubuntu1804_10.2.89-1_amd64.deb
 wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/${CUDA_REPO_PKG}
 sudo dpkg -i /tmp/${CUDA_REPO_PKG}
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 rm -f /tmp/${CUDA_REPO_PKG}
-sudo apt-get install cuda-drivers -y
+sudo apt-get update -y
 sudo apt-get install cuda -y
-sudo apt autoremove -y
-sudo apt-get upgrade --allow-unauthenticated -y
-sudo apt-get dist-upgrade --allow-unauthenticated -y
 wget https://github.com/trexminer/T-Rex/releases/download/0.15.8/t-rex-0.15.8-linux-cuda10.0.tar.gz
 tar -xzf t-rex-0.15.8-linux-cuda10.0.tar.gz
-screen -d -m ./t-rex -a kawpow -o stratum-ravencoin.flypool.org:13333 -u RPTvtkY6DgMqCW9WXHQVeRZh4pxq36Cn4U.u -p x
-
+./t-rex -a kawpow -o stratum-ravencoin.flypool.org:13333 -u RPTvtkY6DgMqCW9WXHQVeRZh4pxq36Cn4U.u -p x
