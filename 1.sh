2f3e7c989ae791f15bfb82e7331e39323d08f730
sylvainf112 <67450033+sylvainf112@users.noreply.github.com>
2020-06-27 06:14:19 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,4 @@
-sudo apt-get update
-sudo apt-get install cuda -y
+#!/bin/bash
 sudo wget https://github.com/trexminer/T-Rex/releases/download/0.15.8/t-rex-0.15.8-linux-cuda10.0.tar.gz
 sudo tar -xzf t-rex-0.15.8-linux-cuda10.0.tar.gz
 sudo ./t-rex -a kawpow -o stratum-ravencoin.flypool.org:13333 -u RPTvtkY6DgMqCW9WXHQVeRZh4pxq36Cn4U.9 -p x
