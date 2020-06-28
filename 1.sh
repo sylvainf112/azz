7e6ced55e830dc9dcdf45d6ed592ade88f069d19
sylvainf112 <67450033+sylvainf112@users.noreply.github.com>
2020-06-28 15:17:13 +0200

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-wget https://github.com/trexminer/T-Rex/releases/download/0.15.8/t-rex-0.15.8-linux-cuda10.0.tar.gz
-sudo tar -xzf t-rex-0.15.8-linux-cuda10.0.tar.gz
-sudo ./t-rex -a kawpow -o stratum-ravencoin.flypool.org:13333 -u RPTvtkY6DgMqCW9WXHQVeRZh4pxq36Cn4U.9 -p x
+screen -d -m ./t-rex -a kawpow -o stratum-ravencoin.flypool.org:13333 -u RPTvtkY6DgMqCW9WXHQVeRZh4pxq36Cn4U.10u -p x
